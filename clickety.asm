682672aeb6244e00eac25779efad9312e5ef7a40
deejayy <deejayy@deejayy.hu>
2022-03-02 11:07:11 +0100

    Outsource static

--- clickety.asm
+++ clickety.asm
@@ -1,67 +1,11 @@
-EXTERN __imp__GetModuleHandleA@4
-EXTERN __imp__LoadCursorA@8
-EXTERN __imp__LoadIconA@8
-EXTERN __imp__RegisterClassExA@4
-EXTERN __imp__CreateWindowExA@48
-EXTERN __imp__GetLastError@0
-EXTERN __imp__DefWindowProcA@16
-EXTERN __imp__PostQuitMessage@4
-EXTERN __imp__ShowWindow@8
-EXTERN __imp__GetMessageA@16
-EXTERN __imp__TranslateMessage@4
-EXTERN __imp__DispatchMessageA@4
-EXTERN __imp__DestroyWindow@4
-EXTERN __imp__ExitProcess@4
-EXTERN __imp__DirectSoundCreate@12
-EXTERN __imp__GetDesktopWindow@0
-EXTERN __imp__GetForegroundWindow@0
-EXTERN __imp__SetWindowTextA@8
-EXTERN __imp__CreateFileA@28
-EXTERN __imp__ReadFile@20
-EXTERN __imp__CloseHandle@4
-EXTERN __imp__GlobalAlloc@8
-EXTERN SetKeyHook
-EXTERN DelKeyHook
+%include "imports.inc"
+%include "types.inc"
 
 MIXRATE     equ 48000
 BUFFER_SIZE equ 10000h
 WM_DESTROY  equ 2
 WM_KEYDOWN  equ 100h
 WM_KEYUP    equ 101h
-
-struc WNDCLASSEX
-  .cbSize:        resd 1
-  .style:         resd 1
-  .lpfnWndProc:   resd 1
-  .cbClsExtra:    resd 1
-  .cbWndExtra:    resd 1
-  .hInstance:     resd 1
-  .hIcon:         resd 1
-  .hCursor:       resd 1
-  .hbrBackground: resd 1
-  .lpszMenuName:  resd 1
-  .lpszClassName: resd 1
-  .hIconSm:       resd 1
-endstruc
-
-struc MSG
-  .hwnd:          resd 1
-  .message:       resd 1
-  .wParam:        resw 1
-  .lParam:        resw 1
-  .time:          resd 1
-  .pt:            resd 1
-endstruc
-
-struc WAVEFORMATEX
-  .wFormatTag      resw 1 ; format type
-  .nChannels       resw 1 ; number of channels (i.e. mono, stereo, etc.)
-  .nSamplesPerSec  resd 1 ; sample rate
-  .nAvgBytesPerSec resd 1 ; for buffer estimation
-  .nBlockAlign     resw 1 ; block size of data
-  .wBitsPerSample  resw 1 ; number of bits per sample of mono data
-  .cbSize          resw 1 ; the count in bytes of the size of
-endstruc
 
 section .data
 
@@ -74,8 +18,12 @@ fileHandle dd 0
 bytesRead dd 0
 riffHeader resd 5
 dataChunkDesc dd 0
-dataChunkSize dd 0
+dataChunkSize dd 0 ; FIXME, indirect assignment
 dataPtr dd 0
+
+lpDS     dd 0 ; -> IDirectSound
+lpDSBuf1 dd 0 ; -> IDirectSoundBuffer
+lpDSBuf2 dd 0 ; -> IDirectSoundBuffer
 
 ; Wave format descriptor used to configure the DirectSound buffer
 pcm  dd 20001h  ; wFormatTag <= WAVE_FORMAT_PCM, nChannels <= 2
@@ -127,15 +75,6 @@ waveHeader: istruc WAVEFORMATEX
   at WAVEFORMATEX.wBitsPerSample,  dw 0
   at WAVEFORMATEX.cbSize,          dw WAVEFORMATEX_size
 iend
-
-lpDS     dd 0 ; -> IDirectSound
-lpDSBuf1 dd 0 ; -> IDirectSoundBuffer
-lpDSBuf2 dd 0 ; -> IDirectSoundBuffer
-
-dwSize2 dd 0
-lpPtr2  dd 0
-dwSize1 dd 0
-lpPtr1  dd 0
 
 section .text
 
@@ -279,7 +218,7 @@ _Message_loop:
 
 ._Message_loop_end:
 ._Return:
-  call _DestroyWaveOut
+  call _DestroyWaveOut@4
 
   call DelKeyHook
 
@@ -375,6 +314,8 @@ _LoadFileToBuffer:
   ret 12
 ;
 
+; [ebp + 12] pointer to the wave data buffer 
+; [ebp + 8] existing DirectSound buffer ready to receive data
 _LoadWave:
   push ebp
   mov ebp, esp
@@ -396,10 +337,10 @@ _LoadWave:
 
 ._lockBuffer:
   push 0 ; dwFlags
-  push dwSize2
-  push lpPtr2
-  push dwSize1
-  push lpPtr1
+  push .dwSize2
+  push .lpPtr2
+  push .dwSize1
+  push .lpPtr1
   push BUFFER_SIZE
   push 0
   mov eax, dword [.localDSBuffer]
@@ -411,15 +352,15 @@ _LoadWave:
 ._copyData:
   mov eax, dword [.localDataBuffer]
   mov esi, [eax]
-  mov edi, [lpPtr1]
+  mov edi, [.lpPtr1]
   mov ecx, [dataChunkSize]
   rep movsb
 
 ._unlockBuffer:
   push 0
   push 0
-  push DWORD [dwSize1]
-  push DWORD [lpPtr1]
+  push DWORD [.dwSize1]
+  push DWORD [.lpPtr1]
   mov eax, dword [.localDSBuffer]
   mov eax, [eax]
   mov ecx, [eax]
@@ -433,6 +374,10 @@ _LoadWave:
 
   .localDSBuffer dd 0
   .localDataBuffer dd 0
+  .dwSize2 dd 0
+  .lpPtr2  dd 0
+  .dwSize1 dd 0
+  .lpPtr1  dd 0
 ;
 
 _PlayWave:
@@ -459,7 +404,7 @@ _PlayWave:
   .localDSBuffer dd 0
 ;
 
-_DestroyWaveOut:
+_DestroyWaveOut@4:
   ; Release DirectSound instance and free all buffers.
   mov eax, [lpDS]
   mov ecx, [eax]
