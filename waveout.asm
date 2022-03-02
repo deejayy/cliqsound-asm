d05da27fd1b217aa40afdc99b1cdeda56f9c1668
deejayy <deejayy@deejayy.hu>
2022-03-02 22:43:21 +0100

    Make dll

--- waveout.asm
+++ waveout.asm
@@ -1,8 +1,22 @@
+section .data
+
 %include "imports.inc"
 %include "types.inc"
 
-section .text
+GLOBAL DestroyWaveOut@4
+EXPORT DestroyWaveOut@4
+GLOBAL LoadFileToBuffer@8
+EXPORT LoadFileToBuffer@8
+GLOBAL InitWaveOut@4
+EXPORT InitWaveOut@4
+GLOBAL LoadWave@16
+EXPORT LoadWave@16
+GLOBAL PlayWave@4
+EXPORT PlayWave@4
+
 _hInstance dd 0
+
+section .text
 
 GLOBAL _DllMain@12
 _DllMain@12:
